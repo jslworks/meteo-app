1d6a035f4ab156c488ef8eed9d28ffcd7eac5c7c
jslworks <jslworksprojects@gmail.com>
2022-05-19 16:12:13 +0200

    meter las funciones en tools

--- app/src/components/MeteoSection/BO/tools.js
+++ app/src/components/MeteoSection/BO/tools.js
@@ -3,8 +3,100 @@ import {
   WiDirectionUp, WiDirectionUpRight,
   WiDirectionRight, WiDirectionDownRight,
   WiDirectionDown, WiDirectionDownLeft,
-  WiDirectionLeft, WiDirectionUpLeft
+  WiDirectionLeft, WiDirectionUpLeft,
+  // Weather properties icons
+  WiStrongWind, WiThermometer
 } from 'react-icons/wi'
+import {
+  BsSunrise, BsSunsetFill, BsCloudRain
+} from 'react-icons/bs'
+import { boxStyle, t } from '../../../styles/Box'
+
+const moment = require('moment')
+const { Divider, Box, Grid } = require('@mui/material')
+
+export const showLocation = (locationData) => {
+  return (
+    <Box
+      sx={{ fontSize: '1.55em', fontWeight: 'bold' }}
+    >
+      {locationData.name}, {locationData.country}
+    </Box>
+  )
+}
+
+export const showDate = () => {
+  const allDate = moment().toObject()
+  return (
+    <Box
+      sx={{ fontSize: '1em' }} pl={1} pt={1}
+    >
+      {moment(allDate).format('dddd') + ', ' +
+          allDate.date + ' ' +
+          moment(allDate).format('MMMM') + ' ' +
+          moment(allDate).format('YYYY') + ' - ' +
+          moment(allDate).format('HH:mm')}
+    </Box>
+  )
+}
+
+export const weatherAndTemperature = (current) => {
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={7}>
+          <Box
+            sx={{ fontWeight: 'bold', fontSize: '2.3em', alignContent: 'center' }}
+          > {Math.round(current.temperature)}<span style={{ fontWeight: 'normal', margin: '0.1em', fontSize: '0.4em' }}> ºC </span>
+          </Box>
+        </Grid>
+        <Grid item xs={5}>
+          <Box>
+            <img
+              src={'http://openweathermap.org/img/wn/' +
+                current.icon + '.png'} alt='weather-icon'
+            />
+          </Box>
+          <Box
+            sx={{ fontSize: '0.8em' }}
+          > {current.description}
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export const currentBasicWeather = (current) => {
+  return (
+    <Grid container>
+      <Grid item xs={6}>
+        <Divider />
+        <Box sx={boxStyle}>
+          <WiThermometer color='red' /> {current.maxTemperature} ºC
+        </Box>
+        <Divider />
+        <Box sx={boxStyle}> ?<BsCloudRain /> {t()} {current.rainProbability} % </Box>
+        <Divider />
+        <Box sx={boxStyle}> <BsSunrise /> {t()} {current.sunrise} </Box>
+        <Divider />
+      </Grid>
+      <Grid item xs={6}>
+        <Divider />
+        <Box sx={boxStyle}>
+          <WiThermometer color='blue' />{current.minTemperature} ºC
+        </Box>
+        <Divider />
+        <Box sx={boxStyle}>
+          {current.wind_speed} km/h <WiStrongWind />{showWindDirection(current.wind_direction)}
+        </Box>
+        <Divider />
+        <Box sx={boxStyle}> <BsSunsetFill /> {t()} {current.sunset} </Box>
+        <Divider />
+      </Grid>
+    </Grid>
+  )
+}
 
 export const showWindDirection = (direction) => {
   const size = '2em'
